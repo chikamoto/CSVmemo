16fcdb9d0a1e9d802365aee1f57f71d5b1b9cf52
EC2 Default User <ec2-user@ip-172-31-62-233.ec2.internal>
2021-12-11 23:06:11 +0000

    訂正

--- memo.rb
+++ memo.rb
@@ -6,23 +6,26 @@ require "csv"
    
   if memo_type == 1
    puts "拡張子を除いたファイル名を入力してください"
-   _name = gets.to_s
+   csv_name = gets.to_s.chomp
+   
    puts "メモしたい内容を記入してください"
    puts "完了後、Ctrl + Dを押します"
-   memo_type = gets.to_s
-   CSV.open("test.csv","w") do |test|
-    test << ["#{memo_type}"]
-   end
+   memo_value = gets.to_s.chomp
   
+   CSV.open("#{csv_name}.csv","w") do |text|
+    text << ["#{memo_value}"]
+  end
    
   elsif memo_type == 2
    puts "編集したいファイル名を入力してください"
-   _name = gets.to_s
- 　 puts "編集内容を記入してください"
+   file_name = gets.to_s.chomp
+   
+   puts "編集内容を入力してください"
    puts "完了後、Ctrl + Dを押します"
-   memo_type = gets.chomp
-   memo_type = $stdin.read
-   CSV.open("test.csv","wb") do |test|
-    test << ["#{memo_type}"]
+   edit_value = gets.to_s.chomp
+  
+   CSV.open("#{file_name}.csv","a") do |edit|
+    edit << ["#{edit_value}"]
    end
   end
+ 
